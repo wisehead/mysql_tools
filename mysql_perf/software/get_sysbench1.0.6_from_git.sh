deb72956f04bc177d45e40c44098db23e7d48928
wisehead <alex.chenhui@gmail.com>
2018-06-27 18:28:20 +0800

    new version sysbench.

---
+++ mysql_perf/software/get_sysbench1.0.6_from_git.sh
@@ -0,0 +1,8 @@
+#############################################################
+#   File Name: get_sysbench1.0.6_from_git.sh
+#     Autohor: Hui Chen (c) 2018
+#        Mail: chenhui13@baidu.com
+# Create Time: 2018/06/27-18:27:31
+#############################################################
+#!/bin/sh 
+wget https://github.com/akopytov/sysbench/archive/1.0.6.zip
